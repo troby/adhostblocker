b037f25acfd41e271e433c768acbfcf9b23f5520
Ted Roby <git@sranet.com>
2016-01-22 22:02:51 -0800

    use tempfile to save bad hosts list

--- adhostblocker.py
+++ adhostblocker.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python2.7
-import re, requests, sys, os
+import re, requests, sys, os, tempfile
 
 # bad hosts taken from someonewhocares.org
 url = 'http://someonewhocares.org/hosts/'
-output = 'hosts'
 
-def main():
+def get_blacklist():
+  # quietly die if request cannot be made
   try:
     blacklist = requests.get(url)
 
@@ -14,12 +14,23 @@ def main():
   except:
     sys.exit(0)
 
-  if not os.path.isfile(output):
-    fh = open(output, 'w')
-    for line in blacklist.text.split('\n'):
-      if re.match(r'^127.0.0.1', line):
-        fh.write(line + '\n')
-    fh.close()
+  bad_hosts = []
+  for line in blacklist.text.split('\n'):
+    if re.match(r'^127.0.0.1', line):
+      bad_hosts.append(line)
+  if len(bad_hosts) == 0:
+    sys.exit(0)
+  return bad_hosts
+
+def save_hosts(bad_hosts):
+  (fd, filename) = tempfile.mkstemp(prefix='hosts-', dir=os.path.abspath('.'))
+  for host in bad_hosts:
+    os.write(fd, host + '\n')
+  os.close(fd)
+
+def main():
+  bad = get_blacklist()
+  save_hosts(bad)
 
 if __name__ == '__main__':
   main()
