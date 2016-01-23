5825a9ac3b849ee095691dc24a5af130a5851bae
Ted Roby <git@sranet.com>
2016-01-22 21:37:37 -0800

    change style layout for script portability

--- adhostblocker.py
+++ adhostblocker.py
@@ -1,21 +1,25 @@
-#!/usr/local/bin/python2.7
+#!/usr/bin/env python2.7
 import re, requests, sys, os
 
 # bad hosts taken from someonewhocares.org
 url = 'http://someonewhocares.org/hosts/'
 output = 'hosts'
 
-try:
-  blacklist = requests.get(url)
+def main():
+  try:
+    blacklist = requests.get(url)
 
-  if blacklist.status_code != 200:
+    if blacklist.status_code != 200:
+      sys.exit(0)
+  except:
     sys.exit(0)
-except:
-  sys.exit(0)
 
-if not os.path.isfile(output):
-  fh = open(output, 'w')
-  for line in blacklist.text.split('\n'):
-    if re.match(r'^127.0.0.1', line):
-      fh.write(line + '\n')
-  fh.close()
+  if not os.path.isfile(output):
+    fh = open(output, 'w')
+    for line in blacklist.text.split('\n'):
+      if re.match(r'^127.0.0.1', line):
+        fh.write(line + '\n')
+    fh.close()
+
+if __name__ == '__main__':
+  main()
