b0775b92e81a1460566f5691f91d4e750da5656d
Ted Roby <git@sranet.com>
2016-02-09 12:18:10 -0800

    peek at the Last-Modified header rather than fetching entire page

--- adhostblocker.py
+++ adhostblocker.py
@@ -11,6 +11,17 @@ url = 'http://someonewhocares.org/hosts/hosts'
 system_hosts = '/etc/hosts'
 lf = os.linesep
 
+def peek_blacklist():
+  try:
+    blacklist = requests.head(url)
+
+    if blacklist.status_code == 200:
+      return blacklist.headers['Last-Modified']
+    else:
+      sys.exit(0)
+  except:
+    sys.exit(0)
+
 def get_blacklist():
   # quietly die if request cannot be made
   try:
@@ -26,13 +37,6 @@ def get_blacklist():
 def parse_hosts(blacklist):
   bad_hosts = []
   for line in blacklist.split(lf):
-    if re.match(r'^# Last updated', line):
-      # check if this is the same version we have
-      if hosts_updated(line) is True:
-        bad_hosts.insert(0, line)
-        continue
-      else:
-        sys.exit(0)
     if re.match(r'^127.0.0.1', line):
       bad_hosts.append(line)
 
@@ -41,22 +45,25 @@ def parse_hosts(blacklist):
   else:
     return bad_hosts
 
-def hosts_updated(new_date):
+def hosts_updated(last_modified):
   fh = open(system_hosts, 'r')
-  for line in fh.read().split(lf):
-    if re.match(r'^# Last updated', line):
-      if line == new_date:
-        fh.close()
+  data = fh.read()
+  fh.close()
+  hosts = data.split(lf)
+  for line in hosts:
+    if re.match(r'^# Last updated: .*$', line):
+      this_version = re.sub(r'^# Last updated: (.*)$', '\\1', line)
+      if this_version == last_modified:
         return False
       else:
-        fh.close()
         return True
     # return True if line not found
-    return True
+  return True
 
-def write_to_etc_hosts(bad_hosts):
+def write_to_etc_hosts(last_modified, bad_hosts):
   etc_hosts = open(system_hosts, 'a')
   etc_hosts.write('# adhostblocker' + lf)
+  etc_hosts.write('# Last updated: ' + last_modified + lf)
   for line in bad_hosts:
     etc_hosts.write(line + lf)
   etc_hosts.close()
@@ -79,10 +86,12 @@ def reset_etc_hosts():
     etc_hosts.close()
 
 def main():
-  rawlist = get_blacklist()
-  bad_hosts = parse_hosts(rawlist)
-  reset_etc_hosts()
-  write_to_etc_hosts(bad_hosts)
+  last_modified = peek_blacklist()
+  if hosts_updated(last_modified):
+    rawlist = get_blacklist()
+    bad_hosts = parse_hosts(rawlist)
+    reset_etc_hosts()
+    write_to_etc_hosts(last_modified, bad_hosts)
 
 if __name__ == '__main__':
   main()
